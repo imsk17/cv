f4174245d0240f7359cf526f4290642a36154c08
imsk17 <imsumitkr17@gmail.com>
2025-01-24 22:22:17 +0530

    initial add data

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -9,120 +9,125 @@ import {
 import { GitHubIcon, LinkedInIcon, XIcon } from "@/components/icons";
 
 export const RESUME_DATA = {
-  name: "Bartosz Jarocki",
-  initials: "BJ",
-  location: "Wrocław, Poland, CET",
-  locationLink: "https://www.google.com/maps/place/Wrocław",
+  name: "Sumit Kumar",
+  initials: "SK",
+  location: "New Delhi, India, IST",
+  locationLink: "https://www.google.com/maps/place/Delhi",
   about:
-    "Detail-oriented Full Stack Engineer dedicated to building high-quality products.",
+    "Detail-oriented Blockchain/Full Stack Engineer dedicated to building high-quality products.",
   summary: (
     <>
-      Frontend-focused Full Stack Engineer specializing in high-performance
-      React applications, scalable Node.js services, and real-time collaboration
-      systems. Experienced in technical architecture design and remote team
-      leadership.
+      Blockchain-focused Full Stack Engineer specializing in high-performance
+      decentralized applications, scalable Node.js services, and real-time
+      collaboration systems. Experienced in technical architecture design and
+      remote team leadership.
     </>
   ),
-  avatarUrl: "https://avatars.githubusercontent.com/u/1017620?v=4",
-  personalWebsiteUrl: "https://jarocki.me",
+  avatarUrl:
+    "https://avatars.githubusercontent.com/u/37952008?s=400&u=57ec05a5649bea7fb66ad74aa9768d8b4e09885e&v=4",
+  personalWebsiteUrl: "https://imsk17.org",
   contact: {
-    email: "bartosz.jarocki@hey.com",
-    tel: "+48530213401",
+    email: "imsumitkr17@gmail.com",
+    tel: "+918126378925",
     social: [
       {
         name: "GitHub",
-        url: "https://github.com/BartoszJarocki",
+        url: "https://github.com/imsk17",
         icon: GitHubIcon,
       },
       {
         name: "LinkedIn",
-        url: "https://www.linkedin.com/in/bjarocki/",
+        url: "https://www.linkedin.com/in/imsk17/",
         icon: LinkedInIcon,
       },
       {
         name: "X",
-        url: "https://x.com/BartoszJarocki",
+        url: "https://x.com/imsk_17",
         icon: XIcon,
       },
     ],
   },
   education: [
     {
-      school: "Wrocław University of Technology",
-      degree: "Bachelor's Degree in Control systems engineering and Robotics",
-      start: "2007",
-      end: "2010",
+      school: "Gautam Buddha University",
+      degree: "Bachelor's Degree in Information Technology",
+      start: "2020",
+      end: "2024",
     },
   ],
   work: [
     {
-      company: "Film.io",
-      link: "https://film.io",
-      badges: ["Remote", "React", "Next.js", "TypeScript", "Node.js"],
-      title: "Software Architect",
+      company: "XP Network",
+      link: "https://xp.network",
+      badges: ["Remote", "Blockchain", "Ethereum", "Solidity", "Rust"],
+      title: "Senior Software Engineer",
       logo: ConsultlyLogo,
-      start: "2024",
+      start: "2021",
       end: null,
       description: (
         <>
-          Leading technical architecture of a blockchain-based film funding
-          platform.
+          Leading technical architecture of a blockchain-based multi-chain NFT
+          Bridge.
           <ul className="list-inside list-disc">
             <li>
-              Architecting migration from CRA to Next.js for improved
-              performance, SEO, and DX
+              Developed Smart contracts for chains like Near, Casper, ICP,
+              MultiversX, Secret, CosmWasm, and Solana using Rust, Tezos using
+              Ligo, Aptos using Move
             </li>
             <li>
-              Established release process enabling faster deployments and
-              reliable rollbacks
+              Wrote a generalized client SDK in typescript which can interact
+              with each chain’s bridge contracts.
             </li>
             <li>
-              Implementing system-wide monitoring and security improvements
+              Implemented Decentralized Bridge Validators from scratch which are
+              responsible for validating bridge transactions.
+            </li>
+            <li>
+              Deployed various blockchain nodes, handled CI/CD development of
+              various programs and took up various system administration
+              responsibilities.
             </li>
           </ul>
         </>
       ),
     },
     {
-      company: "Parabol",
-      link: "https://parabol.co",
+      company: "Freelance",
+      link: "https://upwork.com",
       badges: [
         "Remote",
-        "React",
+        "Freelancing",
         "TypeScript",
-        "Node.js",
-        "GraphQL",
-        "Tailwind CSS",
+        "Golang",
+        "Rust",
+        "Python",
       ],
-      title: "Senior Full Stack Developer",
+      title: "Freelancer",
       logo: ParabolLogo,
       start: "2021",
       end: "2024",
       description: (
         <>
-          Senior developer and squad leader for an enterprise agile meeting
-          platform.
+          Worked as a freelancer for various organization on various projects.
           <ul className="list-inside list-disc">
             <li>
-              Built design system with Tailwind CSS, improving development speed
-              and time to market
+              Blockchain Development which involved smart contracts for various
+              clients in Ethereum and other chains.
             </li>
             <li>
-              Implemented engineering practices: PR automation, code review
-              guidelines, and workflows
-            </li>
-            <li>
-              Open source contributions to Relay DevTools and React i18n tooling
+              Web Development which involved designing frontend and backend
+              servers as per client’s demand across various languages including
+              Go, Typescript, Rust, Python etc.
             </li>
           </ul>
         </>
       ),
     },
     {
-      company: "Clevertech",
-      link: "https://clevertech.biz",
-      badges: ["Remote", "React", "TypeScript", "Node.js", "Android", "Kotlin"],
-      title: "Lead Android Developer → Full Stack Developer",
+      company: "FOSS",
+      link: "https://github.com",
+      badges: ["Remote", "Android", "Rust", "Golang"],
+      title: "Open Source Developer",
       logo: ClevertechLogo,
       start: "2015",
       end: "2021",
@@ -132,82 +137,81 @@ export const RESUME_DATA = {
           leading distributed teams.
           <ul className="list-inside list-disc">
             <li>
-              Led frontend team at Evercast, building real-time platform
-              supporting 30+ users per room with HD streaming and collaboration
-              tools
+              Development of AOSP Forks (Pixel Experience/Cosmic OS), Maintained
+              Device Tree for a Xiaomi Device.
             </li>
-            <li>
-              Developed offline-first Android app for DKMS, improving donor
-              registration process
-            </li>
-            <li>
-              Led development teams across multiple successful client projects
-            </li>
+            <li>Multiple FOSS Apps and Libraries on GitHub.</li>
           </ul>
         </>
       ),
-    },
-    {
-      company: "Jojo Mobile",
-      link: "https://bsgroup.eu/",
-      badges: ["On Site", "Android", "Java", "Kotlin"],
-      title: "Android Developer → Lead Android Developer",
-      logo: JojoMobileLogo,
-      start: "2012",
-      end: "2015",
-      description: (
-        <>
-          First Android developer, grew and led a team of 15+ engineers while
-          establishing engineering culture.
-          <ul className="list-inside list-disc">
-            <li>
-              Developed apps for major Polish companies including LOT, Polskie
-              Radio, and Agora
-            </li>
-            <li>Built and mentored high-performing mobile development team</li>
-          </ul>
-        </>
-      ),
-    },
-    {
-      company: "Nokia Siemens Networks",
-      link: "https://www.nokia.com",
-      badges: ["On Site", "C/C++", "LTE", "Agile"],
-      title: "C/C++ Developer",
-      logo: NSNLogo,
-      start: "2010",
-      end: "2012",
-      description:
-        "Developed software for LTE base stations at enterprise scale, gaining strong fundamentals in software architecture, testing practices, and cross-team collaboration.",
     },
   ],
   skills: [
-    "React/Next.js/Remix",
+    "Rust",
+    "Golang",
     "TypeScript",
+    "Kotlin",
+    "Dart",
+    "Python",
+    "Solidity",
+    "Motoko",
+    "Git",
+    "Docker",
+    "Docker Compose",
+    "Linux",
+    "GitHub Actions",
+    "CI/CD Pipelines",
+    "PostgreSQL",
+    "SQLite",
+    "MySQL",
+    "MongoDB",
+    "Ethereum",
+    "TON",
+    "Cosmwasm",
+    "Near",
+    "MultiversX",
+    "Solana",
+    "Casper",
+    "Substrate",
+    "Dfinity",
+    "Axum Web",
+    "Gin-Go",
+    "Bun",
+    "Node",
+    "Elysia",
+    "MikroORM",
+    "FastAPI",
+    "Express",
+    "Diesel",
+    "SolidJS",
+    "React/Next",
     "Tailwind CSS",
-    "Design Systems",
-    "WebRTC",
-    "WebSockets",
-    "Node.js",
-    "GraphQL",
-    "Relay",
-    "System Architecture",
-    "Remote Team Leadership",
+    "shadcn/ui",
+    "ROM Development",
+    "Flutter",
+    "Jetpack Compose",
+    "Elixir",
+    "Gleam",
+    "Parsers/Compilers",
+    "RabbitMQ",
+    "SDL",
+    "TG Bot API",
+    "Discord Bot API",
   ],
   projects: [
     {
-      title: "Monito",
-      techStack: ["TypeScript", "Next.js", "Browser Extension", "PostgreSQL"],
+      title: "Formula",
+      techStack: ["Rust", "Diesel", "ethers-rs", "PostgreSQL"],
       description:
-        "Browser extension for debugging web applications. Includes taking screenshots, screen recording, E2E tests generation and generating bug reports",
+        "Built a tool to listen for Ethereum NFT events on multiple chains concurrently to record NFT movements and list NFTs of user. Written in Rust, it perfectly utilizes the great concurrency provided via Tokio.",
       logo: MonitoLogo,
       link: {
-        label: "monito.dev",
-        href: "https://monito.dev/",
+        label: "formula",
+        href: "https://github.com/imsk17/formula",
       },
     },
     {
-      title: "Consultly",
+      title: "Posiedon",
       techStack: [
         "TypeScript",
         "Next.js",
@@ -219,22 +223,33 @@ export const RESUME_DATA = {
         "Redis",
       ],
       description:
-        "Platform for online consultations with real-time video meetings and scheduling",
+        "Built a toy blockchain in Rust to learn how a blockchain works. It uses RocksDB as database. Based on PoW Model, and UTXO Style Transaction.",
       logo: ConsultlyLogo,
       link: {
-        label: "consultly.com",
-        href: "https://consultly.com/",
+        label: "github.com",
+        href: "https://github.com/imsk17/posiedon",
       },
     },
     {
-      title: "Minimalist CV",
+      title: "Oak-log",
       techStack: ["TypeScript", "Next.js", "Tailwind CSS"],
       description:
-        "An open source minimalist, print friendly CV template with a focus on readability and clean design. >9k stars on GitHub",
+        "An open source logger for the oak web server on deno. Plug and play logging for Oak.",
       logo: MonitoLogo,
       link: {
-        label: "Minimalist CV",
-        href: "https://github.com/BartoszJarocki/cv",
+        label: "Deno Land",
+        href: "https://deno.land/x/oak_log@v0.3.0",
+      },
+    },
+    {
+      title: "Quicssh-rs",
+      techStack: ["Rust", "QUIC", "HTTP3"],
+      description:
+        "A QUIC/HTTP3 Proxy for SSH Connections that can be used without patching the server using Rust.",
+      logo: MonitoLogo,
+      link: {
+        label: "Github",
+        href: "https://github.com/imsk17/quicssh-rs",
       },
     },
   ],
